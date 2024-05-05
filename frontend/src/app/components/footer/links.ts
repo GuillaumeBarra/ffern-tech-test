9b839db4eedbe843b3d0fc3c3eb172b62e7978ec
Guillaume Barra <guillaume.barra2@gmail.com>
2024-05-05 21:01:49 +0100

    feat: add footer links

---
+++ frontend/src/app/components/footer/links.ts
@@ -0,0 +1,12 @@
+export const productLinks = ['How it works', 'Pricing', 'About', 'Contact'];
+export const companyLinks = [
+  'Terms of Service',
+  'Privacy Policy',
+  'Cookie Policy',
+];
+export const socialLinks = [
+  { name: 'Instagram', icon: '/instagram.svg' },
+  { name: 'Facebook', icon: '/facebook.svg' },
+  { name: 'TikTok', icon: '/tiktok.svg' },
+  { name: 'Pinterest', icon: '/pinterest.svg' },
+];
