5dae7a68ffae1083ad7e472a30ea1d68a868b525
Guillaume Barra <guillaume.barra2@gmail.com>
2024-05-05 21:02:58 +0100

    feat: add success toast component

---
+++ frontend/src/app/components/toast/SuccessToast.tsx
@@ -0,0 +1,13 @@
+export const SuccessToast = () => {
+    return (
+        <div className="p-4 bg-successLight border border-success w-full rounded-md space-y-4">
+            <div>
+                Thank you!
+            </div>
+            <div>
+                Your new fragrance will be with you shortly.
+            </div>
+        </div>
+    )
+}
+
